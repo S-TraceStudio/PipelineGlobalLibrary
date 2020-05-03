db6e52d5971b2458a8261a36aedfb1e582083c4d
Franck Chateau <franck.chateau@arutua.com>
2020-05-03 12:00:49 +0800

    test title

--- vars/notifyBuildOnSlack.groovy
+++ vars/notifyBuildOnSlack.groovy
@@ -1,11 +1,7 @@
 import groovy.json.JsonBuilder
 import groovy.json.JsonSlurper
 
-def call(String title, String text, String textColor) 
+def call(String titleText, String messageText, String textColor) 
 {
-    //json='{"attachments":[{"color":"' + color + '","title":"' + title + '","text":"' + text + '","mrkdwn_in":["text","pretext"]}]}'      
-    //def response = ["curl", "-X", "POST", "-H", "Content-Type: application/json", "--data", "${json}", "https://hooks.slack.com/services/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"].execute().text
-    //println response    
-
-    slackSend color: textColor, message: "Slack message"
+    slackSend color: textColor, message: messageText, title: titleText
 }
