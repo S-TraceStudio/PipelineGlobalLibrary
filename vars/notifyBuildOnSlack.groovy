e1c3aaf16c5d31f56f28af54e2d8f801313623a3
Franck Chateau <franck.chateau@arutua.com>
2020-05-03 09:00:59 +0800

    Adding new hook for S-Trace Studio

--- vars/notifyBuildOnSlack.groovy
+++ vars/notifyBuildOnSlack.groovy
@@ -4,6 +4,6 @@ import groovy.json.JsonSlurper
 def call(String title, String text, String color) 
 {
     json='{"attachments":[{"color":"' + color + '","title":"' + title + '","text":"' + text + '","mrkdwn_in":["text","pretext"]}]}'      
-    def response = ["curl", "-k", "-X", "POST", "-H", "Content-Type: application/json", "--data", "${json}", "https://hooks.slack.com/services/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"].execute().text
+    def response = ["curl", "-k", "-X", "POST", "-H", "Content-Type: application/json", "--data", "${json}", "https://hooks.slack.com/services/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"].execute().text
     println response    
 }
