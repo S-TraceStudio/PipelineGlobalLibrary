df5e362b265ac1c0ef9c19155e2bd494b21a1928
Franck Chateau <franck.chateau@arutua.com>
2020-05-03 10:33:00 +0800

    new test

--- vars/notifyBuildOnSlack.groovy
+++ vars/notifyBuildOnSlack.groovy
@@ -3,7 +3,9 @@ import groovy.json.JsonSlurper
 
 def call(String title, String text, String color) 
 {
-    json='{"attachments":[{"color":"' + color + '","title":"' + title + '","text":"' + text + '","mrkdwn_in":["text","pretext"]}]}'      
-    def response = ["curl", "-X", "POST", "-H", "Content-Type: application/json", "--data", "${json}", "https://hooks.slack.com/services/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"].execute().text
-    println response    
+    //json='{"attachments":[{"color":"' + color + '","title":"' + title + '","text":"' + text + '","mrkdwn_in":["text","pretext"]}]}'      
+    //def response = ["curl", "-X", "POST", "-H", "Content-Type: application/json", "--data", "${json}", "https://hooks.slack.com/services/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"].execute().text
+    //println response    
+
+    slackSend color: "good", message: "Slack message"
 }
