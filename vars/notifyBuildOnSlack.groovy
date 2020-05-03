01c5f2ebcfff4b6390bd7c6fc9d226dda2433ee0
Franck Chateau <franck.chateau@arutua.com>
2020-05-03 12:41:18 +0800

    correcting error

--- vars/notifyBuildOnSlack.groovy
+++ vars/notifyBuildOnSlack.groovy
@@ -3,7 +3,32 @@ import groovy.json.JsonSlurper
 
 def call( String messageTitle, String messageText, String textColor) 
 {
-    jsonAttachement='{"attachments":[{"color":"' + textColor + '","title":"' + messageTitle + '","text":"' + messageText + '","mrkdwn_in":["text","pretext"]}]}'      
+    jsonAttachement='[{"color":"' + textColor + '","title":"' + messageTitle + '","text":"' + messageText + '","mrkdwn_in":["text","pretext"]}]'      
+
+    def attachmenPayload = [[
+            fallback: "Project execution ",
+            color: colorCode,
+            fields:[
+                [
+                    title: "${project}",
+                    value: "Build ",
+                    short: false
+                ],
+                [  
+                    title: "Branch",
+                    value: "Branch Name",
+                    short: true
+                ],
+                [  
+                    title: "Environment",
+                    value: "abc:xyz",
+                    short: true
+                ]
+            ]
+        ]]
+// Send notifications
+
+
 
     slackSend(attachments: jsonAttachement)
 }
