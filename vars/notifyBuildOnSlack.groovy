09ab6c7fd66acb6486df80b1e1176e4f37fe807c
Franck Chateau <franck.chateau@arutua.com>
2020-05-03 19:19:38 +0800

    WIP

--- vars/notifyBuildOnSlack.groovy
+++ vars/notifyBuildOnSlack.groovy
@@ -3,29 +3,27 @@ import groovy.json.JsonSlurper
 
 def call( String messageTitle, String messageText, String textColor) 
 {
-  //  jsonAttachement='[{"color":"' + textColor + '","title":"' + messageTitle + '","text":"' + messageText + '"}]'      
-
     def attachmenPayload = [[
-            fallback: "Project execution ",
-            color: textColor,
-            fields:[
-                [
-                    title: "Project",
-                    value: "Build ",
-                    short: false
-                ],
-                [  
-                    title: "Branch",
-                    value: "Branch Name",
-                    short: true
-                ],
-                [  
-                    title: "Environment",
-                    value: "abc:xyz",
-                    short: true
-                ]
+        fallback: "Project execution",
+        color: textColor,
+        fields:[
+            [
+                title: "Project",
+                value: "Build",
+                short: false
+            ],
+            [  
+                title: "Branch",
+                value: "Branch Name",
+                short: true
+            ],
+            [  
+                title: "Environment",
+                value: "abc:xyz",
+                short: true
             ]
-        ]]
+        ]
+    ]]
 
     slackSend(color: textColor, attachments: new JsonBuilder(attachmenPayload).toPrettyString() )
 }
